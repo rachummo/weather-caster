3050478afad224dd3043236ae07d89f70e740982
rachummo <80019555+rachummo@users.noreply.github.com>
2023-07-26 14:45:46 -0700

    Update script.js format -- added more comments + rearranged/organized code

--- script.js
+++ script.js
@@ -6,36 +6,22 @@ var castformForms = {
    normal:'https://www.professorlotus.com/Sprites/Castform.gif'
 };
 
+// Global vars
 var cityID = 5327684;
 var updateFreq = 1000 * 60 * 15;
 var brightFreq = 1000 * 60;
 var currWeather = 'dummy';
 
+
+// -------------------------------------              CHANGING CASTFORM              -------------------------------------
+
 // Change Castform's form based on the current weather
  function changeCastform(newWeather) {
-      // console.log(currWeather, newWeather);
       if (newWeather != currWeather) {
          document.getElementById('curr-castform').src = castformForms[newWeather];
          currWeather = newWeather;
       }
   };
-
-// Gradually change opacity of castform once it's nightime or morning
-function changeBrightness(initialBright) {
-   if (initialBright == 1) {
-      var brightnessFactor = -0.01;
-   } else {
-      var brightnessFactor = 0.01;
-   }
-   var currBright = initialBright;
-   var interval = setInterval(function () {
-      currBright += brightnessFactor;
-      document.getElementById('curr-castform').style.filter = "brightness(" + currBright + ")";
-      if (currBright <= 0 || currBright >= 1) {
-         clearInterval(interval);
-      }
-   }, brightFreq);
-}
 
 // Using API, check the current weather in prep of changing Castform's appearance
 async function checkWeather(cityID) {
@@ -46,7 +32,6 @@ async function checkWeather(cityID) {
       const response = await fetch(apiURL + cityID + '&units=imperial' +   '&appid=' + key);
       
       // API response (indexable dict)
-      //console.log(await response.json());
       var data = await response.json();
       var temp = data['main']['temp'];
       console.log("This is the current temp: " + temp + " °F");
@@ -65,7 +50,29 @@ async function checkWeather(cityID) {
       }
 }
 
-// Sleep functionality
+// -------------------------------------              TIME-BASED BRIGHTNESS SETTING              -------------------------------------
+
+// Gradually change opacity of castform once it's night-time or morning
+function changeBrightness(initialBright) {
+   // Night-time -> dim display
+   if (initialBright == 1) {
+      var brightnessFactor = -0.01;
+   // Dawn/morning -> brighten display
+   } else {
+      var brightnessFactor = 0.01;
+   }
+   var currBright = initialBright;
+   var interval = setInterval(function () {
+      currBright += brightnessFactor;
+      document.getElementById('curr-castform').style.filter = "brightness(" + currBright + ")";
+      // Stop changing brightness levels once finished
+      if (currBright <= 0 || currBright >= 1) {
+         clearInterval(interval);
+      }
+   }, brightFreq);
+}
+
+// Sleep functionality for checkTime()
 function sleep(ms) {
    return new Promise(resolve => setTimeout(resolve, ms));
 }
@@ -74,17 +81,18 @@ function sleep(ms) {
 async function checkTime() {
    // Rpi bootup, loads html and script, could be any time
    var currTime = new Date();
+   
    // We want to start dimming at ie. 12 am
    var nightTime = new Date();
-   
    nightTime.setDate(nightTime.getDate() + 1);
    nightTime.setHours(0, 0, 0);
+   
    // We want to start raising brightness at ie. 6 am
    var morningTime = new Date();
    morningTime.setDate(morningTime.getDate() + 1);
    morningTime.setHours(6, 0, 0);
       
-   // Find number of ms until it's nighttime and morning time
+   // Find number of ms until it's night-time and morning time
    var untilNight = nightTime - currTime;
    var untilDawn = morningTime - currTime;
       
@@ -92,17 +100,19 @@ async function checkTime() {
    console.log('Waiting ' + untilNight + ' miliseconds until 12 AM');
    await sleep(untilNight);
    changeBrightness(1);
-   // Run changeBrightnesss at night every 24 hrs
+   // Begin running changeBrightnesss at night every 24 hrs
    setInterval(changeBrightness, 1000 * 60 * 60 * 24, 1);
    
    console.log('Waiting ' + untilDawn + ' miliseconds until 6 AM');
    await sleep(untilDawn);
    changeBrightness(0);
-   // Run changeBrightnesss at morning every 24 hrs
+   // Begin running changeBrightnesss at morning every 24 hrs
    setInterval(changeBrightness, 1000 * 60 * 60 * 24, 0);
 }
 
-// On bootup, run functions
+
+// -----------------------------------                STARTUP FUNCTIONS                -----------------------------------
+// Note: run checkWeather upon bootup so to not wait for interval delay initially
 window.onload = () => {
    console.log("hello");
    checkWeather(cityID);
